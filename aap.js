7bffc1ac3bc87f4ea5724df6059368d581edc606
Monetry <Monetry>
2025-08-19 12:53:21 +0000

    trigger CI/CD pipeline

--- aap.js
+++ aap.js
@@ -3,3 +3,4 @@ const app = express();
 const port = 3000;
 app.get('/', (req, res) => res.send('Hello from CI/CD!'));
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
+res.send('Hello from Updated CI/CD Pipeline!');
